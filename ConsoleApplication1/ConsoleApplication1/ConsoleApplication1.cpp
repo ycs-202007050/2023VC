c08c497f6d634e0b095d939f50e2391924132c01
ycs-202007050 <80020102+ycs-202007050@users.noreply.github.com>
2023-09-12 15:18:24 +0900

    조신우

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+void print(char c) {
+    switch (c) {
+    case 'q':
+        printf("qwer 화이팅");
+        break;
+    case 'w':
+        printf("과제 너무 좋다");
+        break;
+    case 'e':
+        printf("담주부턴 과제량 3배다");
+        break;
+    case 'r':
+        printf("행복합니다");
+        break;
+    default:
+        printf("오류입니다. 다시 입력해주세요.");
+
+    }
+}
+
+int main()
+{   
+    printf("행복한 VC++");
+    char a;
+    
+    while (1) {
+        printf("\ncommand>");
+        scanf_s("%c", &a);
+    }
+
+}
+
